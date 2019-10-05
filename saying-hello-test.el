9c2401935744d2e5e4973a9b070c70c6c5e19727
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-05 16:05:16 +0200

    Add another test

--- saying-hello-test.el
+++ saying-hello-test.el
@@ -2,9 +2,18 @@
 (setq load-path (cons "." load-path))
 (require 'saying-hello)
 
+(defconst a-message "some message")
+
 (describe "saying-hello"
   (describe "sh--format-hello"
     (it "correctly formats the input value"
       (expect (sh--format-hello "Dave")
-              :to-equal "Hello, Dave, nice to meet you!"))))
+              :to-equal "Hello, Dave, nice to meet you!")))
+  (describe "sh--print-hello-message"
+    (it "prints the output using the messages"
+      (spy-on 'message)
+      (sh--print-hello-message a-message)
+      (expect 'message :to-have-been-called-with a-message))))
+
+
 
--- saying-hello.el
+++ saying-hello.el
@@ -3,6 +3,10 @@
 (defun sh--format-hello (name)
   (concat "Hello, " name ", nice to meet you!"))
 
+(defun sh--print-hello-message (text)
+  (message text))
+
+
 
 
 
