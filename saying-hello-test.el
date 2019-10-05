1241fd8e120460bd1ec8545e5ec701d9554b8495
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-05 16:24:26 +0200

    TDD driven "Hello, world!": ✓

--- saying-hello-test.el
+++ saying-hello-test.el
@@ -4,7 +4,7 @@
 
 (defconst a-message "some message")
 
-(describe "saying-hello"
+(describe "Utility functions"
   (describe "sh--format-hello"
     (it "correctly formats the input value"
       (expect (sh--format-hello "Dave")
@@ -13,7 +13,20 @@
     (it "prints the output using the messages"
       (spy-on 'message)
       (sh--print-hello-message a-message)
-      (expect 'message :to-have-been-called-with a-message))))
+      (expect 'message :to-have-been-called-with a-message)))
+  (describe "sh--ask-name"
+    (it "uses read-string with the appropriate prompt"
+      (spy-on 'read-string :and-call-fake (lambda (_) "Dave"))
+      (sh--ask-name)
+      (expect 'read-string
+              :to-have-been-called-with "What is your name? "))))
 
-
-
+(describe "say-hello"
+  (it "read the correct input and writes the correct output"
+    (spy-on 'read-string :and-call-fake (lambda (message) "Roger"))
+    (spy-on 'message)
+    (say-hello)
+    (expect 'read-string
+            :to-have-been-called-with "What is your name? ")
+    (expect 'message
+            :to-have-been-called-with "Hello, Roger, nice to meet you!")))
--- saying-hello.el
+++ saying-hello.el
@@ -1,10 +1,22 @@
 
 
+(defun sh--print-hello-message (text)
+  (message text))
+
 (defun sh--format-hello (name)
   (concat "Hello, " name ", nice to meet you!"))
 
-(defun sh--print-hello-message (text)
-  (message text))
+(defun sh--ask-name ()
+  (read-string "What is your name? "))
+
+(defun say-hello ()
+  "Say hello to %var%"
+  (interactive)
+  (sh--print-hello-message
+   (sh--format-hello
+    (sh--ask-name))))
+
+
 
 
 
